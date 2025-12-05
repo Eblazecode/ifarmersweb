75b25d6ed082cdf6eaac4a70c1ee1f4f6e5ce8a5
Eblazecode <dynablaze@gmail.com>
2025-12-05 11:20:49 +0100

    v 1.0 fixed things logo

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { toast } from '@/components/ui/use-toast';
 
+
 interface AppContextType {
   sidebarOpen: boolean;
   toggleSidebar: () => void;
